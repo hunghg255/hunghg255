376ee91d39bb150ab6de5ec431b044caf75082d4
hunghg255 <giahunghust@gmail.com>
2023-09-13 11:59:26 +0700

    chore: update get project

---
+++ update-projects.js
@@ -0,0 +1,110 @@
+const axios = require("axios").default;
+require("dotenv/config");
+
+const repository = process.env.GH_REPOSITORY;
+const token = process.env.GH_TOKEN;
+
+const GIST = `https://gist.githubusercontent.com/hunghg255/ee79b03819fd5f9a2a92cba8839d5942/raw/projects.json?t=${Date.now()}`;
+
+(async () => {
+  try {
+    const { encoding, content } = (
+      await axios.get(`https://api.github.com/repos/${repository}/readme`, {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      })
+    ).data;
+
+    const readmeContent = Buffer.from(content, encoding).toString();
+
+    const { sha } = (
+      await axios.get(
+        `https://api.github.com/repos/${repository}/contents/README.md`
+      )
+    ).data;
+
+    const { data } = await axios.get(GIST)
+
+    const formattedPosts = Object.keys(data).map((key) => {
+      const projects = data[key];
+
+      return `
+<h2 align='center'>${key}</h2>
+
+<table>
+  <thead align="center">
+    <tr>
+      ${
+        Object.keys(projects[0]).map((key) => {
+          return `<th>${key}</th>`
+        }).join("\n")
+      }
+    </tr>
+  </thead>
+  <tbody align="left">
+  ${projects.map((project) => {
+    return `<tr>
+      <th>
+        ${project.Name}
+      </th>
+      <th>
+        <a href="${project.Github}" target="_blank">Github</a>
+      </th>
+      <th>
+        <a href="${project.Npm}" target="_blank">NPM</a>
+      </th>
+      <th>${project.Summary}</th>
+      <th>
+        ${
+          project.Technology.map((tech) => {
+            return `<img src="${tech}" alt="badge">`
+          }).join("\n")
+        }
+      </th>
+      <th>
+        ${
+          project.Achievement.map((achievement) => {
+            return `<img src="${achievement}" alt="badge">`
+          }).join("\n")
+        }
+      </th>
+    </tr>`;
+  }).join("\n")}
+  </tbody>
+</table>
+      `;
+    }).join("\n");
+
+    const result = readmeContent.replace(
+      /<!-- start-projects -->\n(.|\n)*<!-- end-projects -->/gm,
+      `<!-- start-projects -->\n${formattedPosts}\n<!-- end-projects -->`
+    );
+
+    if (result !== readmeContent) {
+      console.log("Different posts list");
+
+      await axios.put(
+        `https://api.github.com/repos/${repository}/contents/README.md`,
+        {
+          message: "Update README with latest posts",
+          content: Buffer.from(result).toString("base64"),
+          sha,
+        },
+        {
+          headers: {
+            Accept: "application/vnd.github.v3+json",
+            Authorization: `token ${token}`,
+          },
+        }
+      );
+    } else {
+      console.log("No new blog posts");
+    }
+
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
