334644cdf2a937c78996ec3b98f01528c25d26a5
hunghg255 <giahunghust@gmail.com>
2023-11-24 11:22:57 +0700

    chore: update

--- update-projects.js
+++ update-projects.js
@@ -1,10 +1,20 @@
-const axios = require("axios").default;
-require("dotenv/config");
+const axios = require('axios').default;
+require('dotenv/config');
 
 const repository = process.env.GH_REPOSITORY;
 const token = process.env.GH_TOKEN;
 
 const GIST = `https://gist.githubusercontent.com/hunghg255/ee79b03819fd5f9a2a92cba8839d5942/raw/projects.json?t=${Date.now()}`;
+
+const locale = {
+  handbook: 'Handbook',
+  github: 'Github',
+  npm: 'NPM',
+  star: 'Stars',
+  last_commit: 'Last Commit',
+  download: 'Download',
+  version: 'Version',
+};
 
 (async () => {
   try {
@@ -24,37 +34,40 @@ const GIST = `https://gist.githubusercontent.com/hunghg255/ee79b03819fd5f9a2a92c
       )
     ).data;
 
-    const { data } = await axios.get(GIST)
-    console.log({data});
+    const { data } = await axios.get(GIST);
+    console.log({ data });
 
-    const formattedPosts = Object.keys(data).map((key) => {
-      const projects = data[key];
+    const formattedPosts = Object.keys(data)
+      .map((key) => {
+        const projects = data[key];
 
-      return `
+        return `
 <h2 align='center'>${key}</h2>
 
 <table>
   <thead align="center">
     <tr>
-      ${
-        Object.keys(projects[0]).map((key) => {
-          return `<th>${key}</th>`
-        }).join("\n")
-      }
+      ${Object.keys(projects[0])
+        .map((key) => {
+          return `<th>${locale[key]}</th>`;
+        })
+        .join('\n')}
     </tr>
   </thead>
   <tbody align="left">
-  ${projects.map((project) => {
-
-    return `<tr>
+  ${projects
+    .map((project) => {
+      return `<tr>
       <th>
         ${project.handbook}
       </th>
       <th>
-        <a href="${project.github}" target="_blank">Github</a>
+        <a href="${project.github}" target="_blank">#Github</a>
       </th>
       <th>
-        <a href="${project.npm || project.marketplace}" target="_blank">${project.npm ? "NPM" : "Marketplace"}</a>
+        <a href="${project.npm || project.marketplace}" target="_blank">${
+        project.npm ? '#Npm' : '#Marketplace'
+      }</a>
       </th>
       <th>
         <img src="${project.star}" alt="" />
@@ -69,11 +82,13 @@ const GIST = `https://gist.githubusercontent.com/hunghg255/ee79b03819fd5f9a2a92c
         <img src="${project.version}" alt="" />
       </th>
     </tr>`;
-  }).join("\n")}
+    })
+    .join('\n')}
   </tbody>
 </table>
       `;
-    }).join("\n");
+      })
+      .join('\n');
 
     const result = readmeContent.replace(
       /<!-- start-projects -->\n(.|\n)*<!-- end-projects -->/gm,
@@ -81,24 +96,24 @@ const GIST = `https://gist.githubusercontent.com/hunghg255/ee79b03819fd5f9a2a92c
     );
 
     if (result !== readmeContent) {
-      console.log("Different posts list");
+      console.log('Different posts list');
 
       await axios.put(
         `https://api.github.com/repos/${repository}/contents/README.md`,
         {
-          message: "Update README with latest posts",
-          content: Buffer.from(result).toString("base64"),
+          message: 'Update README with latest posts',
+          content: Buffer.from(result).toString('base64'),
           sha,
         },
         {
           headers: {
-            Accept: "application/vnd.github.v3+json",
+            Accept: 'application/vnd.github.v3+json',
             Authorization: `token ${token}`,
           },
         }
       );
     } else {
-      console.log("No new blog posts");
+      console.log('No new blog posts');
     }
 
     process.exit(0);
